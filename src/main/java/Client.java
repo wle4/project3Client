c483afd6b612e4acbf4ae0e22a9683e6c0db1fb2
Stephen Harayo <sharay2@uic.edu>
2024-04-20 00:26:49 -0500

    12:26am - OK with comments

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -9,14 +9,20 @@ import java.util.function.Consumer;
 
 public class Client extends Thread{
 
-
+	// Declaring member variables for socket, input/output streams, and a callback function
 	Socket socketClient;
+	ObjectOutputStream out; // java library
+	ObjectInputStream in; // java library
 
-	ObjectOutputStream out;
-	ObjectInputStream in;
+	private Consumer<Serializable> callback; // be able to print this out in front end
 
-	private Consumer<Serializable> callback;
+	// Constructor for the Client class, which takes a Consumer<Serializable> callback
+	// Consumer<Serializable> is part of Java's STANDARD LIBRARY, import java.io.Serializable;...
 
+		// <Serializable> means it can be any object or type which implements serializable
+	// Consumer has two functions:
+	// accept()- which takes <Serializable> as parameter
+	// then takes that parameter into Consumer
 	Client(Consumer<Serializable> call){
 
 		callback = call;
@@ -32,10 +38,14 @@ public class Client extends Thread{
 		}
 		catch(Exception e) {}
 
+		// Infinite loop to keep receiving messages from the server
 		while(true) {
 
 			try {
-				String message = in.readObject().toString();
+			// Reading a Serializable object from the ObjectInputStream and converting it to a String
+			//String message = in.readObject().toString();
+				Message message = (Message) in.readObject();
+			// Passing the received message to the callback function
 				callback.accept(message);
 			}
 			catch(Exception e) {}
@@ -43,6 +53,7 @@ public class Client extends Thread{
 
 	}
 
+	// Method to send data to the server
 	public void send(String receiver, String sender, String msg) {
 
 		try {
@@ -52,7 +63,7 @@ public class Client extends Thread{
 			Message message = new Message(receiver, sender, msg); // make a new message object
 			out.writeObject(message); // send the message object to the server
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
